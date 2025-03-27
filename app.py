274096eeb5066f2e504f7c0b33e7baf81bc90b0a
CheLoV <chelovyan.dima@gmail.com>
2025-03-27 16:01:44 +0300

    Fix: use correct host and port for Render

--- app.py
+++ app.py
@@ -23,4 +23,9 @@ def index():
     return render_template('index.html', data=data, view=view, search=search)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    import os
+
+    if __name__ == '__main__':
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host='0.0.0.0', port=port)
